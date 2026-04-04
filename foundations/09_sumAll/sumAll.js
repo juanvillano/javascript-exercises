27a656fac6936271ed31d28686c8253fdb488ecf
juanvillano <104040385+juanvillano@users.noreply.github.com>
2026-04-04 11:54:48 -0400

    complete sumAll function exercise
    completed exercise nine of js foundations
    which takes two integers and returns the
    sum of every integer between and including
    them

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(value1, value2) {
+    let result = 0;
 
+  if (typeof value1 != 'number' || typeof value2 != 'number' || value1 < 0 || value2 < 0 || !Number.isInteger(value1) || !Number.isInteger(value2) ) {
+    return 'ERROR';
+  } else {
+    if (value1 < value2) {
+      result = value1;
+      for (let i = value2; i > value1; i--) {
+        result += i;
+      }
+    } else if (value1 > value2) {
+      result = value2;
+      for (let i = value1; i > value2; i--) {
+        result += i;
+      }
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
