e315a00e3a8a8c7ab3ab2995ce02c4055d883b4b
juanvillano <104040385+juanvillano@users.noreply.github.com>
2026-04-03 20:58:32 -0400

    complete exercise six
    completed exercise six of js foundations
    which takes a two args, a string and a number
    and repeats the string the number indicated

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let result = '';
 
+    if (num < 0) {
+        return 'ERROR';
+    }
+        
+    if (str == '' || num == 0) {
+        return '';
+    }
+
+    for (let i=0; i < num; i++) {
+        result += str;
+    }
+    return result;
 };
 
 // Do not edit below this line
