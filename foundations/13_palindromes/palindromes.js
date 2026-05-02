d25a61b14ddb3bbcabf25e6230ed1e8cdc51b0cd
juanvillano <104040385+juanvillano@users.noreply.github.com>
2026-05-02 08:58:56 -0400

    complete palindrome exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let originalString = str.replace(/[^a-z0-9]/gi, "").toLowerCase();
+  let reversedString = originalString.split("").reverse().join("");
 
+  return originalString === reversedString;
 };
 
 // Do not edit below this line
