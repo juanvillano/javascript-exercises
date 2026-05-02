90f8c5a45ee0d016867e6956ed94e5dbd2acc775
juanvillano <104040385+juanvillano@users.noreply.github.com>
2026-05-02 08:58:35 -0400

    complete fibonacci exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (typeof n === "string") n = parseInt(n);
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+    if (n < 0) return "OOPS";
 
+    
+    let previous = 1;
+    let current = 1; 
+
+    for(let i = 2; i<n; i++) {
+        let next = current + previous;
+        previous = current;
+        current = next;
+    }
+    return current;
 };
 
 // Do not edit below this line
