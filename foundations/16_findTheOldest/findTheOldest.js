9270bb4c0712831cbc5ada374930f279303ffc3a
juanvillano <104040385+juanvillano@users.noreply.github.com>
2026-05-03 07:49:00 -0400

    complete find the oldest exercise

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.map(person => {
+    if(!person.hasOwnProperty("yearOfDeath")) {
+        person["yearOfDeath"] = (new Date()).getFullYear();
+        return person;
+    } 
+    return person;
+    }).reduce((oldest, person) => {
+        let old = (oldest.yearOfDeath - oldest.yearOfBirth) > (person.yearOfDeath - person.yearOfBirth) ? oldest : person;
+        return old;
+    }, {});
 };
 
 // Do not edit below this line
